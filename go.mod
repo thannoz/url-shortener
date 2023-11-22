76d450c839ec9bb19cd2d4330c509f8829abb835
thannoz <carlos-7@hotmail.de>
2023-11-22 20:14:33 +0100

    Started implementing the main logic of the service

--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module hex
 
 go 1.19
+
+require go.step.sm/cli-utils v0.8.0
+
+require (
+	github.com/leodido/go-urn v1.1.0 // indirect
+	github.com/teris-io/shortid v0.0.0-20220617161101-71ec9f2aa569 // indirect
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli v1.22.14 // indirect
+	gopkg.in/dealancer/validate.v2 v2.1.0
+)
--- shortener/logic.go
+++ shortener/logic.go
@@ -1,1 +1,40 @@
 package shortener
+
+import (
+	"errors"
+	"time"
+
+	"github.com/teris-io/shortid"
+	"go.step.sm/cli-utils/errs"
+	"gopkg.in/dealancer/validate.v2"
+)
+
+var (
+	ErrRedirectNotFound = errors.New("Redirect Not Found")
+	ErrRedirectInvalid  = errors.New("Redirect Invalid")
+)
+
+type redirectService struct {
+	redirectRepo RedirectRepository
+}
+
+// Find calls the repository and returns its find method
+func (r *redirectService) Find(code string) (*Redirect, error) {
+	return r.redirectRepo.Find(code)
+}
+
+// Store validates the redirect struct.
+// Generates a short code and time and call the store method of RedirectRepository
+func (r *redirectService) Store(redirect *Redirect) error {
+	if err := validate.Validate(redirect); err != nil {
+		return errs.Wrap(ErrRedirectInvalid, "service.Redirect.Store")
+	}
+
+	redirect.Code = shortid.MustGenerate()
+	redirect.CreatedAt = time.Now().UTC().Unix()
+	return r.redirectRepo.Store(redirect)
+}
+
+func NewRedirectService(redirectRepo RedirectRepository) RedirectService {
+	return &redirectService{redirectRepo}
+}
